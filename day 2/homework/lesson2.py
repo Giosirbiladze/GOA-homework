178b2469c28d5643acb2611079d7a4fe245aedfe
Giosirbiladze <gio.mamaladze.02@list.ru>
2024-05-28 00:56:15 +0400

    goa-academy

--- day 2/homework/lesson2.py
+++ day 2/homework/lesson2.py
@@ -6,4 +6,4 @@ age = 16
 
 height = 184
 
-print(name + " " + surname + " "  + str(age) + " " + str(height))
+print("hello my name is",name + " " + "my surname is",surname + " "  + "i am",str(age) + " " + "i am",str(height),"cm")
