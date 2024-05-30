5598cf8164a6d7f698a97991b697e5bd66f8e58a
Giosirbiladze <gio.mamaladze.02@list.ru>
2024-05-30 13:30:13 +0400

    goa-academy

---
+++ day 6/homework/classwork.py
@@ -0,0 +1,19 @@
+num1 = int(input("write first number"))
+num2 = int(input("write second number"))
+num3 = int(input("write third number"))
+num4 = int(input("write fourth number"))
+#ცვლადს ვანიჭებ მნიშვნელობას ინფუთის დახმარებით
+#სტრინგს ვცვლი ინტეჯერით int ის გამოყენებით
+
+print(num1+num2*num3/num4)
+#ვასრულებ არითმეთიკულ მოქმედებებს
+#ვტვირთავ ტერმინალში პრინტის დახმარებით
+
+
+user_firstname = input("Please enter your firstname: ")
+user_lastname = input("Please enter your lastname: ")
+user_age = input("Please enter your age: ")
+user_email = input("Please enter your email: ")
+
+
+print("Your inputed information is:",user_firstname,user_lastname, user_age, user_email)
---
+++ day 6/homework/homework.py
@@ -0,0 +1,27 @@
+user_firstname = input("Please enter your firstname: ")
+congratulatory_message = input("Write a greeting message: ")
+
+
+fathers_age = input("my fathers age is: ")
+
+
+mothers_age = input("my mothers age is: ")
+
+
+sisters_age = input("my sisters age is: ")
+
+
+
+print(int(fathers_age) + 20)
+
+print(int(mothers_age) + 20)
+
+print(int(sisters_age) + 20)
+
+
+user_firstname = input("Please enter your firstname: ")
+user_lastname = input("Please enter your lastname: ")
+user_age = input("Please enter your age: ")
+user_email = input("Please enter your email: ")
+user_address = input("Please enter your address: ")
+print("your firstname:"+ "" +user_firstname + " " + "your lastname:" + "" +user_lastname + " " + "your age:" + "" +user_age + " " + "your email:" + "" +user_email + " " + "your address:" + "" +user_address)
